7565002c8b84262d1b3f32d98c59bac1c76a1527
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-09-10 15:05:47 -0600

    Updated dependencies.

--- Generator/Package.swift
+++ Generator/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Generator",
     dependencies: [
-        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .upToNextMinor(from: Version(0, 8, 0))),
+        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .upToNextMinor(from: Version(0, 10, 1))),
         ],
     targets: [
         .target(
