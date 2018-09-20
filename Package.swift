6a0f7659cb71cfbbd94af102f048a3237213933a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-09-20 10:46:11 -0600

    Swift 4.2 (#6)
    * Updated SDGCornerstone.
    
    * Updated tools version.

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version:4.1
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "Generator",
     dependencies: [
-        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .upToNextMinor(from: Version(0, 10, 1))),
+        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .exact(Version(0, 11, 0))),
         ],
     targets: [
         .target(
             name: "generate",
             dependencies: [
-                .productItem(name: "SDGCornerstone", package: "SDGCornerstone")
+                .product(name: "SDGCornerstone", package: "SDGCornerstone")
             ],
             path: "Generator/Sources/generate"),
         ]
